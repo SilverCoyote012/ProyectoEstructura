c538ee2d9c9d58de4f32af9010232c6d6ba0e90c
Axongther <noa2006117@gmail.com>
2024-09-26 21:44:42 -0600

    Add Java project configuration and dependency manageme

--- src/com/silver/main/conexionDB.java
+++ src/com/silver/main/conexionDB.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class conexionDB {
-    private static final String URL = "jdbc:mysql://localhost:3306/students";
+    private static final String URL = "jdbc:mysql://localhost:3306/PROYECTO__";
     private static final String USER = "root";
-    private static final String PASSWORD = "Alumno123";
+    private static final String PASSWORD = "Nicooa.6652/";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
--- src/com/silver/main/test.java
+++ src/com/silver/main/test.java
@@ -1,7 +1,6 @@
 package com.silver.main;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -9,9 +8,10 @@ public class test {
     public static void main(String[] args) {
         try (Connection connection = conexionDB.getConnection()) {
             Statement statement = connection.createStatement();
+            System.err.println("Conexión exitosa");
             
         } catch (SQLException e) {
-            e.printStackTrace();
+            e.printStackTrace(); // Mostrar error en consola
         }
     }
 }
